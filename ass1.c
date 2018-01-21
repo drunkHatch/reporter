113879e5c043194f8740e0f6ce8b1e871ab8f6d4
taijie <taijie@ualberta.ca>
2018-01-21 16:55:27 -0700

    Update ass1.c

--- ass1.c
+++ ass1.c
@@ -8,50 +8,54 @@
 #include <sys/types.h>
 
 void change_reporter(){
-	DIR *dir;
-	struct dirent *dp;
-	char * file_name;
-	int file_type;
-	dir = opendir(".");
-
-	while ((dp=readdir(dir)) != NULL) {
-			if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
-			{
-					//printf("wrong_name: \"%s\"\n",file_name);
-			}
-			else {
-				file_name = dp->d_name; // use it
-				file_type = dp->d_type;
-				printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
-																(file_type == 4) ?  "directory" :
-																(file_type == 8) ?  "regular" :
-																(1) ? "other" : "???");
-			}
-	}
+    DIR *dir;
+    struct dirent *dp;
+    char * file_name;
+    int file_type;
+    dir = opendir(".");
+    
+    while ((dp=readdir(dir)) != NULL) {
+        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
+        {
+            //printf("wrong_name: \"%s\"\n",file_name);
+        }
+        else {
+            file_name = dp->d_name; // use it
+            file_type = dp->d_type;
+            printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
+                   (file_type == 4) ?  "directory" :
+                   (file_type == 8) ?  "regular" :
+                   (1) ? "other" : "???");
+        }
+    }
 }
 
-void sig_handler(int signo){
-
-	if (signo == SIGINT){
-		system("date");
-		change_reporter();
-	}
-	if (signo == SIGALRM){
-		system("date");
-		change_reporter();
-	}
+static void sig_handler(int signo){
+    
+    if (signo == SIGINT){
+        system("date");
+        change_reporter();
+    }
+    if (signo == SIGALRM){
+        system("date");
+        change_reporter();
+    }
 }
 
 int main(void)
 {
+    struct sigaction sigaction_usr;
+    sigaction_usr.sa_flags = 0;
+    sigaction_usr.sa_handler = sig_handler;
 
-	signal(SIGALRM, sig_handler);
 
-	signal(SIGINT, sig_handler);
-
-	while(1)
-		{
-		alarm(5);
-		sleep(5);}
-	return 0;
+    sigaction(SIGALRM, &sigaction_usr, NULL);
+    
+    sigaction(SIGINT, &sigaction_usr, NULL);
+    
+    while(1)
+    {
+        alarm(5);
+        sleep(5);}
+    return 0;
 }
