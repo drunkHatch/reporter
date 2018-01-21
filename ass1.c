18d3dd1c8bb8b1f2ab4ef90bc88ea6b861c1643c
Yuqi Zhang <yuqi8@cs.ualberta.ca>
2018-01-21 16:22:23 -0700

    exit program unexpectedly

--- ass1.c
+++ ass1.c
@@ -1,22 +1,50 @@
-#include<stdio.h>
-#include<signal.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+
+void change_reporter(){
+	DIR *dir;
+	struct dirent *dp;
+	char * file_name;
+	int file_type;
+	dir = opendir(".");
+
+	while ((dp=readdir(dir)) != NULL) {
+			if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
+			{
+					//printf("wrong_name: \"%s\"\n",file_name);
+			}
+			else {
+				file_name = dp->d_name; // use it
+				file_type = dp->d_type;
+				printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
+																(file_type == 4) ?  "directory" :
+																(file_type == 8) ?  "regular" :
+																(1) ? "other" : "???");
+			}
+	}
+}
 
 void sig_handler(int signo){
 
-	if (signo == SIGINT)
-		{printf("received SIGINT\n");}
-	if (signo == SIGALRM)
+	if (signo == SIGINT){
 		system("date");
-
-
+		change_reporter();
+	}
+	if (signo == SIGALRM){
+		system("date");
+		change_reporter();
+	}
 }
 
 int main(void)
 {
-	
+
 	signal(SIGALRM, sig_handler);
 
 	signal(SIGINT, sig_handler);
@@ -27,4 +55,3 @@ int main(void)
 		sleep(5);}
 	return 0;
 }
-
--- readdir.c
+++ readdir.c
@@ -8,57 +8,25 @@
 
 int main()
 {
-    DIR *dir; 
+    DIR *dir;
     struct dirent *dp;
     char * file_name;
-    //char file_type;
     int file_type;
-    dir = opendir("."); // what is it for?
+    dir = opendir(".");
 
     while ((dp=readdir(dir)) != NULL) {
-        //printf("debug: %s\n", dp->d_name);
         if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
         {
-            printf("wrong_name: \"%s\"\n",file_name);
+            //printf("wrong_name: \"%s\"\n",file_name);
         } else {
             file_name = dp->d_name; // use it
             file_type = dp->d_type;
-            //printf("file_name: \"%s\" file_type: \"%u\"\n",file_name, file_type);
-            /*
-            DT_UNKNOWN = 0,
-            DT_FIFO = 1,
-            DT_CHR = 2,
-            DT_DIR = 4,
-            DT_BLK = 6,
-            DT_REG = 8,
-            DT_LNK = 10,
-            DT_SOCK = 12,
-            DT_WHT = 14
-            */
             printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
-                             (file_type == 0) ?  "unknown" :
-                             (file_type == 1) ? "FIFO" :
-                             (file_type == 2) ?  "char dev" :
                              (file_type == 4) ?  "directory" :
-                             (file_type == 6) ?  "block dev" :
                              (file_type == 8) ?  "regular" :
-                             (file_type == 10) ?  "symlink" :
-                             (file_type == 12) ? "socket" :
-                             (file_type == 14) ?  "DT_WHT" : "???");
+                             (1) ? "other" : "???");
         }
     }
     closedir(dir);
-
-    //printf("_DIRENT_HAVE_D_TYPE: \"%u\"\n", _DIRENT_HAVE_D_TYPE);
-    /*
-    printf("DT_REG: \"%u\"\n", DT_REG);
-    printf("DT_DIR: \"%u\"\n", DT_DIR);
-    printf("DT_FIFO: \"%u\"\n", DT_FIFO);
-    printf("DT_SOCK: \"%u\"\n", DT_SOCK);
-    printf("DT_CHR: \"%u\"\n", DT_CHR);
-    printf("DT_BLK: \"%u\"\n", DT_BLK);
-    printf("DT_LNK: \"%u\"\n", DT_LNK);
-    */
-
     return 0;
 }
