dd084cd7952d9b7d5b610cb492f410e2dbf9642c
Yuqi Zhang <yuqi8@cs.ualberta.ca>
2018-01-21 17:24:45 -0700

    alarm clock fixed

--- ass0.c
+++ ass0.c
@@ -1,30 +1,62 @@
-#include<stdio.h>
-#include<signal.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
 
-void sig_handler(int signo){
+void change_reporter(){
+    DIR *dir;
+    struct dirent *dp;
+    char * file_name;
+    int file_type;
+    dir = opendir(".");
 
-	if (signo == SIGINT)
-		{printf("received SIGINT\n");}
-	if (signo == SIGALRM)
-		system("date");
+    while ((dp=readdir(dir)) != NULL) {
+        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
+        {
+            //printf("wrong_name: \"%s\"\n",file_name);
+        }
+        else {
+            file_name = dp->d_name; // use it
+            file_type = dp->d_type;
+            printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
+                   (file_type == 4) ?  "directory" :
+                   (file_type == 8) ?  "regular" :
+                   (1) ? "other" : "???");
+        }
+    }
+		closedir(dir);
+}
 
+static void sig_handler(int signo){
 
+    if (signo == SIGINT){
+        system("date");
+        change_reporter();
+    }
+    if (signo == SIGALRM){
+        system("date");
+        change_reporter();
+    }
 }
 
 int main(void)
 {
-	
-	signal(SIGALRM, sig_handler);
+    struct sigaction sigaction_usr;
+    sigaction_usr.sa_flags = 0;
+    sigaction_usr.sa_handler = sig_handler;
 
-	signal(SIGINT, sig_handler);
 
-	while(1)
-		{
-		alarm(5);
-		sleep(5);}
-	return 0;
+    sigaction(SIGALRM, &sigaction_usr, NULL);
+
+    sigaction(SIGINT, &sigaction_usr, NULL);
+
+    while(1)
+    {
+        alarm(5);
+        sleep(5);}
+    return 0;
 }
-
--- ass1.c
+++ ass1.c
@@ -8,50 +8,64 @@
 #include <sys/types.h>
 
 void change_reporter(){
-	DIR *dir;
-	struct dirent *dp;
-	char * file_name;
-	int file_type;
-	dir = opendir(".");
+    DIR *dir;
+    struct dirent *dp;
+    char * file_name;
+    int file_type;
+    dir = opendir(".");
 
-	while ((dp=readdir(dir)) != NULL) {
-			if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
-			{
-					//printf("wrong_name: \"%s\"\n",file_name);
-			}
-			else {
-				file_name = dp->d_name; // use it
-				file_type = dp->d_type;
-				printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
-																(file_type == 4) ?  "directory" :
-																(file_type == 8) ?  "regular" :
-																(1) ? "other" : "???");
-			}
-	}
+    while ((dp=readdir(dir)) != NULL) {
+        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
+        {
+            //printf("wrong_name: \"%s\"\n",file_name);
+        }
+        else {
+            file_name = dp->d_name; // use it
+            file_type = dp->d_type;
+            printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
+                   (file_type == 4) ?  "directory" :
+                   (file_type == 8) ?  "regular" :
+                   (1) ? "other" : "???");
+        }
+    }
+		closedir(dir);
 }
 
-void sig_handler(int signo){
+static void sig_handler(int signo){
 
-	if (signo == SIGINT){
-		system("date");
-		change_reporter();
-	}
-	if (signo == SIGALRM){
-		system("date");
-		change_reporter();
-	}
+    if (signo == SIGINT){
+        system("date");
+        change_reporter();
+    }
+    if (signo == SIGALRM){
+        system("date");
+        change_reporter();
+				signal (SIGALRM, sig_handler);
+    }
+		/*reset handler reason
+		https://cboard.cprogramming.com/linux-programming/150239-when-re-enable-signal-handlers.html
+		The signal() function defines the handling of the next received signal only,
+		after which the default handling is reinstated.
+		So it is necessary for the signal handler to call signal()
+		if the program needs to continue handling signals using a non-default handler.
+		*/
 }
 
 int main(void)
 {
+    //struct sigaction sigaction_usr;
+    //sigaction_usr.sa_flags = 0;
+    //sigaction_usr.sa_handler = sig_handler;
 
-	signal(SIGALRM, sig_handler);
 
-	signal(SIGINT, sig_handler);
+		signal(SIGALRM, sig_handler);
 
-	while(1)
-		{
-		alarm(5);
-		sleep(5);}
-	return 0;
+		signal(SIGINT, sig_handler);
+
+    while(1)
+    {
+        alarm(1);
+        sleep(1);
+		}
+    return 0;
 }
