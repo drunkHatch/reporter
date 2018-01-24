4c49a8e2f05700d061c02b89ef7512c40c4f18aa
drunkHatch <32227266+drunkHatch@users.noreply.github.com>
2018-01-23 17:15:57 -0700

    Add files via upload

--- ass1.c
+++ ass1.c
@@ -6,6 +6,25 @@
 #include <dirent.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h> //optional
+// inode -> change name, modify 1231412
+/*
+filename, type, time, inode
+if delete and create same file:
+stay the same:
+filename -> same?
+diff:
+filename + inode -> same?
+type&time -> sign
+
+struct name,type,time
+struct name,type,time,inode
+*/
+
+int fstat(int fd, struct stat *statbuf);
 
 void change_reporter(){
     DIR *dir;
@@ -13,7 +32,7 @@ void change_reporter(){
     char * file_name;
     int file_type;
     dir = opendir(".");
-    
+
     while ((dp=readdir(dir)) != NULL) {
         if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
         {
@@ -28,34 +47,47 @@ void change_reporter(){
                    (1) ? "other" : "???");
         }
     }
+		closedir(dir);
 }
 
 static void sig_handler(int signo){
-    
+
     if (signo == SIGINT){
         system("date");
         change_reporter();
+				exit(1);
     }
-    if (signo == SIGALRM){
+    else if (signo == SIGALRM){
         system("date");
         change_reporter();
+				signal (SIGALRM, sig_handler);
     }
+    else if (signo == SIGUSR1){
+        system("date");
+        change_reporter();
+        signal(SIGUSR1, sig_handler);
+    }
+		/*reset handler reason
+		https://cboard.cprogramming.com/linux-programming/150239-when-re-enable-signal-handlers.html
+		The signal() function defines the handling of the next received signal only,
+		after which the default handling is reinstated.
+		So it is necessary for the signal handler to call signal()
+		if the program needs to continue handling signals using a non-default handler.
+		*/
 }
 
 int main(void)
 {
-    struct sigaction sigaction_usr;
-    sigaction_usr.sa_flags = 0;
-    sigaction_usr.sa_handler = sig_handler;
+		signal(SIGALRM, sig_handler);
+    signal(SIGUSR1, sig_handler);
+		signal(SIGINT, sig_handler);
+		system("date");
+		change_reporter();
 
-
-    sigaction(SIGALRM, &sigaction_usr, NULL);
-    
-    sigaction(SIGINT, &sigaction_usr, NULL);
-    
     while(1)
     {
-        alarm(5);
-        sleep(5);}
+        alarm(3);
+        sleep(3);
+		}
     return 0;
 }
