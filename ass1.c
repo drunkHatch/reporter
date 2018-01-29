7e100c958a294d83413aa01a2bb5d7a7f04153e0
drunkHatch <32227266+drunkHatch@users.noreply.github.com>
2018-01-29 14:07:57 -0700

    Add files via upload

--- ass1.c
+++ ass1.c
@@ -7,103 +7,87 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <fcntl.h> //optional
 #include <sys/inotify.h>
 #include <inttypes.h>
-
-//#include <cstdio>
-#include <signal.h>
 #include <limits.h>
-#include <sys/inotify.h>
-#include <fcntl.h>
-//#include <iostream>
-
-#include <sys/inotify.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <inttypes.h>
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-// inode -> change name, modify 1231412
-/*
-filename, type, time, inode
-if delete and create same file:
-stay the same:
-filename -> same?
-diff:
-filename + inode -> same?
-type&time -> sign
 
-struct name,type,time
-struct name,type,time,inode
-*/
+
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define EVENT_BUF_LEN ( 1024 * ( EVENT_SIZE + 16 ) )
+#define FILE_INFO_LEN ( sizeof(FINFO))
 
-int length, i = 0;
+typedef struct{
+  char name[256];
+  int type; //0->dir, 1->reg, 2->other
+  ino_t inode;
+} FINFO;
+
+int length;
 int fd;
 int wd;
+fd_set rfds;
+struct timeval tv;
+int retval;
 char buffer[EVENT_BUF_LEN];
-//FD_ZERO(&inputs)
-//FD_SET(fd,&inputs);
+DIR *dir;
 
-int fstat(int fd, struct stat *statbuf);
+int old_dir_num = 0; //old directory file #
+int new_dir_num = 0; // new directory file #
+int temp_dir_num = 0; // temp directory file #
+FINFO *old_file_info; // old files info
+FINFO *new_file_info; // new files info
+FINFO *temp_file_info; // temp files info, w8 to be transfered to new_file_info
+FINFO *change_file_info; // temp files info, w8 to be transfered to new_file_info
 
-void change_reporter(){
-    DIR *dir;
+FINFO get_member(FINFO *dir_list, char *file_name, int length){
+  FINFO fail = {"miss", -1, 0};
+  for (int i = 0; i < length; i++) {
+    if (strcmp(dir_list[i].name, file_name) == 0){
+      return dir_list[i];
+    }
+  }
+  return fail;
+}
+
+
+int dir_grabber(DIR *dir){
     struct dirent *dp;
     char * file_name;
     int file_type;
-    dir = opendir(".");
+    ino_t file_inode;
+    int file_num = 0;
+    FINFO current_node;
 
     while ((dp=readdir(dir)) != NULL) {
-        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
-        {
-            //printf("wrong_name: \"%s\"\n",file_name);
-        }
+        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )  {}
         else {
-            file_name = dp->d_name; // use it
-            file_type = dp->d_type;
-            printf("file_name: \"%s\" file_type: \"%s\" \n",file_name,
-                   (file_type == 4) ?  "directory" :
-                   (file_type == 8) ?  "regular" :
-                   (1) ? "other" : "???");
+            file_num++;
+            strcpy(current_node.name, dp->d_name);
+            current_node.type = dp->d_type;
+            current_node.inode = dp->d_ino;
+            temp_file_info = (FINFO *)realloc(temp_file_info, file_num * FILE_INFO_LEN);
+            temp_file_info[file_num - 1] = current_node;
         }
     }
-		closedir(dir);
+    temp_dir_num = file_num;
+    return file_num;
 }
 
 void reporter(){
-  //int check;
-///////////////////////////////
-  // result = select(FD_SETSIZE, &testfds, (fd_set *)0, (fd_set *)0, (struct timeval *) 0);
-  // /***********************////////
-  fd_set rfds;
-  struct timeval tv;
-  int retval;
+  int i = 0;
+  FINFO old;
+  FINFO new;
 
   FD_ZERO(&rfds);
   FD_SET(fd, &rfds);
-
   tv.tv_sec = 0;
-  tv.tv_usec = 1;
-
+  tv.tv_usec = 10;
   retval = select(fd+1, &rfds, NULL, NULL, &tv);
 
-  printf( "1\n");
-
-  /************************/
-  //check = select()
-  //printf("%s\n", buffer);
   if(retval > 0) {
     length = read( fd, buffer, EVENT_BUF_LEN);
   }
@@ -111,125 +95,280 @@ void reporter(){
     return;
   }
 
-  /*checking for error*/
   if ( length < 0 ) {
     perror( "read" );
   }
-//noob
-////////////////////////////////
+
   /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
-  while ( i < length ) {struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     if ( event->len ) {
+  while ( i < length ) {
+    struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+    if ( event->len ) {
+      old = get_member(old_file_info, event->name, old_dir_num);
+      new = get_member(new_file_info, event->name, new_dir_num);
       if ( event->mask & IN_CREATE ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "New directory %s created.\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+            }
+            else{
+              print_crt(new);
+            }
+          }
+          else{//in old but not in new
+          }
         }
-        else {
-          printf( "New file %s created.\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+            print_crt(new);
+          }
+          else{//not in old and not in new
+          }
         }
       }
+
       else if ( event->mask & IN_DELETE ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "Directory %s deleted.\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+            }
+            else{
+              print_del(old);
+            }
+          }
+          else{//in old but not in new
+            print_del(old);
+          }
         }
-        else {
-          printf( "File %s deleted.\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+          }
+          else{//not in old and not in new
+          }
         }
       }
       else if ( event->mask & IN_MODIFY ) {
-        if ( event->mask & IN_ISDIR ) {
-          printf( "Directory %s modified.\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+              print_mod(new);
+            }
+            else{
+            }
+          }
+          else{//in old but not in new
+          }
         }
-        else {
-          printf( "File %s modified.\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+          }
+          else{//not in old and not in new
+          }
         }
       }
       else if ( event->mask & IN_ATTRIB ) {
-        printf( "cookie: %" PRIu32 " !!\n", event->cookie );
-        if ( event->mask & IN_ISDIR ) {
-          printf( "* %s .\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+              print_mod(new);
+            }
+            else{
+            }
+          }
+          else{//in old but not in new
+          }
         }
-        else {
-          printf( "* %s .\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+          }
+          else{//not in old and not in new
+          }
         }
       }
       else if ( event->mask & IN_MOVED_FROM ) {
-        printf( "cookie: %" PRIu32 " !!\n", event->cookie );
-        if ( event->mask & IN_ISDIR ) {
-          printf( "IN_MOVED_FROM %s .\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+            }
+            else{
+              print_del(new);
+            }
+          }
+          else{//in old but not in new
+            print_del(new);
+          }
         }
-        else {
-          printf( "IN_MOVED_FROM %s .\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+          }
+          else{//not in old and not in new
+          }
         }
       }
       else if ( event->mask & IN_MOVED_TO ) {
-        printf( "cookie: %" PRIu32 " !!\n", event->cookie );
-
-        if ( event->mask & IN_ISDIR ) {
-          printf( "IN_MOVED_TO %s .\n", event->name );
+        if (old.type != -1){// if in old
+          if (new.type != -1){//if in old & new
+            if (old.type == new.type && old.inode == new.inode){
+            }
+            else{
+              print_crt(new);
+            }
+          }
+          else{//in old but not in new
+          }
         }
-        else {
-          printf( "IN_MOVED_TO %s .\n", event->name );
+        else{// not in old
+          if (new.type != -1){//if not in old but in new
+            print_crt(new);
+          }
+          else{//not in old and not in new
+          }
         }
       }
     }
     i += EVENT_SIZE + event->len;
   }
-  /*removing the “/tmp” directory from the watch list.*/
-   //inotify_rm_watch( fd, wd );
-
-
-  /*closing the INOTIFY instance*/
-   //close( fd );
 }
+
+//#define	SIG_BLOCK     0		 /* Block signals.  */
+//#define	SIG_UNBLOCK   1		 /* Unblock signals.  */
+//#define	SIG_SETMASK   2		 /* Set the set of blocked signals.  */
+
 static void sig_handler(int signo){
+    sigset_t signal_set;
+
+    sigemptyset(&signal_set);
 
     if (signo == SIGINT){
+        sigaddset(&signal_set, SIGALRM);
+        sigaddset(&signal_set, SIGUSR1);
+        sigprocmask(0, &signal_set, NULL);
         system("date");
-        //reporter();
+        dir = opendir(".");
+        dir_grabber(dir);
+        closedir(dir);
+        struct_move();
+        reporter();
+        sigprocmask(1, &signal_set, NULL);
 				exit(1);
     }
     else if (signo == SIGALRM){
+        sigaddset(&signal_set, SIGINT);
+        sigaddset(&signal_set, SIGUSR1);
+        sigprocmask(0, &signal_set, NULL);
         system("date");
+        dir = opendir(".");
+        dir_grabber(dir);
+        closedir(dir);
+        struct_move();
         reporter();
 				signal (SIGALRM, sig_handler);
+        alarm(3);
+        sigprocmask(1, &signal_set, NULL);
+
     }
     else if (signo == SIGUSR1){
+        sigaddset(&signal_set, SIGALRM);
+        sigaddset(&signal_set, SIGINT);
+        sigprocmask(0, &signal_set, NULL);
         system("date");
-        //reporter();
+        dir = opendir(".");
+        dir_grabber(dir);
+        closedir(dir);
+        struct_move();
+        reporter();
         signal(SIGUSR1, sig_handler);
+        sigprocmask(1, &signal_set, NULL);
+
     }
-		/*reset handler reason
+		/*reset handler reason////
 		https://cboard.cprogramming.com/linux-programming/150239-when-re-enable-signal-handlers.html
 		The signal() function defines the handling of the next received signal only,
 		after which the default handling is reinstated.
 		So it is necessary for the signal handler to call signal()
 		if the program needs to continue handling signals using a non-default handler.
-		*/
+		*//////////
 }
-
-int main(void)
+int main()
 {
+    int leng = 0;
+
+
 		signal(SIGALRM, sig_handler);
     signal(SIGUSR1, sig_handler);
 		signal(SIGINT, sig_handler);
 		system("date");
-		//change_reporter();
+    // init all struct array with null pointer
+    old_file_info = (FINFO *)malloc(0);
+    new_file_info = (FINFO *)malloc(0);
+    temp_file_info = (FINFO *)malloc(0);
+    change_file_info = (FINFO *)malloc(0);
 
-    while(1)
-    {
-      // init inotify////
-      // begin watching for 5 sec
-      fd = inotify_init();
+    // init for first report
+    dir = opendir(".");
+		leng = dir_grabber(dir);
+    struct_move();
+    print_dir(new_file_info, new_dir_num);
+    closedir(dir);
 
-      if ( fd < 0 ) {
-        perror( "inotify_init" );
-      }
-
-      wd = inotify_add_watch( fd, ".", IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO | IN_ATTRIB);
-
-      /*************************/
-        alarm(2);
-        sleep(2);
-		}
+    fd = inotify_init();
+    wd = inotify_add_watch( fd, ".", IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO | IN_ATTRIB);
+    if ( fd < 0 ) {
+      perror( "inotify_init" );
+    }
+    alarm(3);
+    while(1){
+      ;
+    }
     return 0;
+}
+
+
+
+void struct_copy(FINFO *dst, FINFO *src,int length){
+  for (int i = 0; i < length; i++) {
+      dst[i].type = src[i].type;
+      dst[i].inode = src[i].inode;
+      strcpy(dst[i].name, src[i].name);
+  }
+}
+
+void print_dir(FINFO *dir, int length){
+  for (int i = 0; i < length; i++) {
+    printf("\"%s\" \"%s\"\n",
+           (dir[i].type == 4) ?  "d" :
+           (dir[i].type == 8) ?  "+" :
+           (1) ? "o" : "???",dir[i].name);
+  }
+}
+
+void print_crt(FINFO a_file){
+  printf("\"%s\" \"%s\"\n",
+         (a_file.type == 4) ?  "d" :
+         (a_file.type == 8) ?  "+" :
+         (1) ? "o" : "???",a_file.name);
+}
+
+void print_del(FINFO a_file){
+  printf("\"%s\" \"%s\"\n",
+         (a_file.type == 4) ?  "-" :
+         (a_file.type == 8) ?  "-" :
+         (1) ? "-" : "???",a_file.name);
+}
+
+void print_mod(FINFO a_file){
+  printf("\"%s\" \"%s\"\n",
+         (a_file.type == 4) ?  "*" :
+         (a_file.type == 8) ?  "*" :
+         (1) ? "*" : "???",a_file.name);
+}
+
+void struct_move(){
+  old_file_info = (FINFO *)realloc(old_file_info, new_dir_num * FILE_INFO_LEN);
+  old_dir_num = new_dir_num;
+  struct_copy(old_file_info, new_file_info, new_dir_num);
+  new_file_info = (FINFO *)realloc(new_file_info, temp_dir_num * FILE_INFO_LEN);
+  new_dir_num = temp_dir_num;
+  struct_copy(new_file_info, temp_file_info, new_dir_num);
+  temp_file_info = (FINFO *)realloc(temp_file_info, 0);
+  temp_dir_num = 0;
 }
